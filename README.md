7e3e9d25ba460a5f9e62103c1be2e0bca678c0bb
kimchild <allcreation@nate.com>
2021-06-05 10:44:13 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# security
+
+* AES 256테스트를 위한 문서
+  + 기능목록
+    - encode
+    - decode
---
+++ src/test/java/aes/EncodingTest.java
@@ -0,0 +1,13 @@
+package aes;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class EncodingTest {
+
+	@Test
+	public void hello() {
+		assertThat(true).isTrue();
+	}
+}
