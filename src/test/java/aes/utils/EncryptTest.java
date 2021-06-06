bcf5b610a117dd7f3a917a68a95c1f05db746326
kimchild <allcreation@nate.com>
2021-06-06 22:20:42 +0900

    feat(EncryptTest.java): url 형식으로 요청이 왔을때 기능추가
    요청이 암화화 하려는 텍스트만 올수도 있지만
    실제 URL을 풀로 보내서 parameter만 암호화 요청할수도 있기 때문에
    둘다를 처리할 수 있는 메소드를 추가한 테스트코드를 추가

--- src/test/java/aes/utils/EncryptTest.java
+++ src/test/java/aes/utils/EncryptTest.java
@@ -60,4 +60,90 @@ public class EncryptTest {
 		System.out.println(plainText);
 		assertThat(plainText).isEqualTo(decryptText);
 	}
+
+	@Test
+	public void URL_PARAMETER_FIND() {
+		// String url = "http://localhost:8080?encParam=dfjkdfjk&core=23jj43&type=abnormal";
+		String url = "encParam=dfjkdfjk&core=23jj43&type=abnormal";
+		String encryptText = null;
+		String decryptText = null;
+		String plainUrl;
+		String encryptParameter;
+
+		EncryptNormal encryptNormal;
+
+		try {
+			// encode
+			encryptNormal = new EncryptNormal();
+
+			int encodeIndex = url.indexOf("?");
+			if (encodeIndex == -1) {
+				encryptText = encryptNormal.getEncryptText(url);
+				/*실제로는 decrypt는 사용 안한다.*/
+				decryptText = encryptNormal.getDecryptText(encryptText);
+				/**/
+			}
+			// 실제 적용시 위 if문에 return 처리하고 아래 if는 삭제
+			if (encodeIndex >= 0) {
+				++encodeIndex;
+				plainUrl = url.substring(0, encodeIndex);
+				encryptParameter = encryptNormal.getEncryptText(url.substring(encodeIndex));
+
+				StringBuilder urlBuilder = new StringBuilder(plainUrl);
+				urlBuilder.append("encParam=").append(encryptParameter);
+				encryptText = urlBuilder.toString();
+
+				/*실제로는 decrypt는 사용 안한다.*/
+				decryptText = encryptNormal.getDecryptText(encryptParameter);
+				urlBuilder = new StringBuilder(plainUrl);
+				urlBuilder.append(encryptNormal.getDecryptText(encryptParameter));
+				decryptText = urlBuilder.toString();
+				/**/
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		assertThat(encryptText).isNotEmpty();
+		assertThat(decryptText).isNotEmpty();
+		assertThat(url).isEqualTo(decryptText);
+
+	}
+
+	@Test
+	public void 파라미터_암호화() {
+		String url = "http://localhost:8080?encParam=dfjkdfjk&core=23jj43&type=abnormal";
+		// String url = "encParam=dfjkdfjk&core=23jj43&type=abnormal";
+		String encryptText = null;
+
+		try {
+			// encode
+			encryptText = getEncryptParameter(url);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+
+		assertThat(encryptText).isNotEmpty();
+
+	}
+
+	private String getEncryptParameter(String url) throws
+		NoSuchPaddingException,
+		NoSuchAlgorithmException,
+		InvalidKeyException,
+		BadPaddingException,
+		InvalidAlgorithmParameterException,
+		IllegalBlockSizeException {
+
+		EncryptNormal encryptNormal = new EncryptNormal();
+		int encodeIndex = url.indexOf("?");
+		if (encodeIndex == -1) {
+			return encryptNormal.getEncryptText(url);
+		}
+
+		final String encryptParameter = encryptNormal.getEncryptText(url.substring(encodeIndex + 1));
+		return url.substring(0, encodeIndex + 1) + "encParam=" + encryptParameter;
+	}
+
 }
